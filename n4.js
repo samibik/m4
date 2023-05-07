41d9cf25fbb2b53fc92ca96f5902c7e0dbf645bd
samibik <wmounirw@gmail.com>
2023-05-07 13:25:38 -0700

    n4.js

---
+++ n4.js
@@ -0,0 +1,269 @@
+window.onload = function(){
+    
+    var seed = {
+        x: (2147483648 * Math.random()) | 0,
+        y: (2147483648 * Math.random()) | 0,
+        z: (2147483648 * Math.random()) | 0,
+        w: (2147483648 * Math.random()) | 0
+    };
+    function randomInt(xors) {
+        var t = xors.x ^ (xors.x << 11);
+        xors.x = xors.y;
+        xors.y = xors.z;
+        xors.z = xors.w;
+        return xors.w = (xors.w^(xors.w>>>19))^(t^(t>>>8));
+    }
+    function random(xors) {
+        return randomInt(xors) / 2147483648;
+    }
+    function shuffle(xs){
+        var v = Object.assign({}, seed);
+        var xs = xs.slice();
+        var ys = [];
+        while(0 < xs.length){
+            var i = Math.abs(randomInt(v)) % xs.length;
+            ys.push(xs[i]);
+            xs.splice(i, 1);
+        }
+        return ys;
+    }
+
+    var colorTuples = shuffle([
+        ["#ffd700", "#ffd700"], 
+        
+    ]);
+
+    var topColors = shuffle(["#04ad8f", "#a6ce48", "#f3a118", "#ea6435", "#17b297", "#e30983", "#2782c4", "#1aa6e7", "#b5b5b5", "#f29905", "#e50011", "#ccdc26", "#a5328d", "#0aaa60", "#91c423", "#f29300", "#ec5f69", "#22b69e", "#e63e9b", "#917220"]);
+
+
+    var topInput = document.querySelector("#top");
+    var middleInput = document.querySelector("#middle");
+    var bottomInput = document.querySelector("#bottom"); 
+
+    var top = document.querySelector(".top");
+    var middle = document.querySelector(".middle");
+    var bottom = document.querySelector(".bottom");
+
+    var foreground = document.getElementById("foreground");
+    var image = document.getElementById("result");
+	
+    
+    var container = document.querySelector(".container");
+    var download = document.getElementById("download");
+
+    var canvas = document.createElement("canvas");
+    var g = canvas.getContext("2d");
+	    
+        
+
+
+    function update(){
+        setTimeout(function(){
+            setText(topInput.value, middleInput.value, bottomInput.value);
+        });
+    }      
+    middleInput.addEventListener("change", update);
+    middleInput.addEventListener("keydown", update);    
+    topInput.addEventListener("change", update);
+    topInput.addEventListener("keydown", update);  
+    bottomInput.addEventListener("change", update);
+    bottomInput.addEventListener("keydown", update);        
+
+    function setText(topText, middleText, bottomText){
+
+
+        
+
+        var topTextSize = 100;
+        var topMiddlePadding = 10;
+        var middleTextSize = 140;
+        var middleBottomPadding = 20;        
+        var bottomTextSize = 40;
+        var margin = 30;
+        var bottomTextLetterSpacing = 20;
+
+        var topTextFont = `normal bold ${topTextSize}px/2 "Kitab"`;
+        var middleTextFont = `normal 500 ${middleTextSize}px/2 Marhey`;
+        var bottomTextFont = `normal bold  ${bottomTextSize}px/2 Kitab`;
+
+        // resize canvas
+		
+        g.font = topTextFont;
+        var topMetrics = g.measureText(topText);
+        g.font = middleTextFont;
+        var middleMetrics = g.measureText(middleText);  
+        g.font = bottomTextFont;
+        var bottomMetrics = g.measureText(bottomText);  
+        canvas.width = margin + Math.max(
+            topMetrics.width, 
+            middleMetrics.width, 
+            bottomMetrics.width + bottomTextLetterSpacing * (bottomText.length - 1)
+        ) + margin;
+        canvas.height = margin + topTextSize + topMiddlePadding + middleTextSize + middleBottomPadding + bottomTextSize + margin;
+
+        // prepare canvas
+        g.save();		
+        g.clearRect(0, 0, canvas.width, canvas.height);
+        g.textBaseline = "top";
+
+
+
+        // stroke top text 
+        function iterate(callback){
+            var xors = Object.assign({}, seed);
+            g.save();
+
+            g.font = topTextFont;        
+            g.fillStyle = "white";
+            g.strokeStyle = "white";
+            g.lineJoin = "round";    
+            g.lineWidth = 20.0;   
+            var metrics = g.measureText(topText);
+            g.translate(margin + (canvas.width - metrics.width - margin * 2) * 0.5, margin);
+            var x = 0;
+            for(var i = 0; i < topText.length; i++){
+                var c = topText.slice(i, i + 1);
+                var rot = random(xors) * 0.2;
+                var metrics = g.measureText(c);
+                g.save();
+                g.translate(metrics.width * 0.5, topTextSize * 0.5);
+                g.rotate(rot);
+                g.translate(-metrics.width * 0.5, -topTextSize * 0.5);
+                callback(i, c);
+                g.restore();
+                g.translate(metrics.width, 0);
+            }
+            g.restore();
+        }
+        g.save();
+        var xors = Object.assign({}, seed);
+        
+
+
+        var topColors = ["#04ad8f", "#a6ce48", "#f3a118", "#ea6435", "#17b297", "#e30983", "#2782c4", "#1aa6e7", "#b5b5b5", "#f29905", "#e50011", "#ccdc26", "#a5328d", "#0aaa60", "#91c423", "#f29300", "#ec5f69", "#22b69e", "#e63e9b", "#917220"];
+
+  
+        iterate(function(i, c){
+            g.shadowColor = "transparent";
+
+            g.strokeText(c, 0, 0);            
+        });
+        iterate(function(i, c){
+            g.shadowColor = "transparent";
+            g.shadowBlur = 10;
+            g.fillStyle = topColors[i % topColors.length];
+            g.fillText(c, 0, 0);
+        });
+
+
+
+
+
+
+        // centerize
+        var metrics = g.measureText(middleText);
+        g.translate((canvas.width - middleMetrics.width) * 0.5, margin + topTextSize + topMiddlePadding);
+
+        // stroke outline
+        g.font = middleTextFont;
+        g.strokeStyle = "#FFDF00";
+        g.lineWidth = 10.0;
+        g.shadowColor = "#000";
+        g.shadowBlur = 10;
+        g.lineCap = "round";
+        g.lineJoin = "round";
+        g.strokeText(middleText, 0, 0);
+		
+		
+        
+        // fill charactors
+        var x = 0;
+        var xors = Object.assign({}, seed);
+        for(var i = 0; i < middleText.length; i++){
+            var c = middleText.slice(x,x =21);
+			var rot = random(xors) * 5.2;
+
+            // base color
+            g.shadowColor = "rgba(0, 0, 0, 0.6)";
+            g.shadowBlur = 10;
+            g.fillStyle = colorTuples[i % colorTuples.length][0];
+            g.fillText(c, 0, 0);
+
+            g.save();
+
+            // clip
+            var rot = random(xors);
+            g.beginPath();
+            g.save();
+            g.translate(middleTextSize * 0.5, middleTextSize * 0.5);            
+            g.rotate(rot);
+            g.translate(-middleTextSize * 0.5, -middleTextSize * 0.5);
+            g.moveTo(-middleTextSize * 2, middleTextSize * 0.5);
+            g.lineTo(middleTextSize * 2, middleTextSize * 0.5);
+            g.lineTo(middleTextSize * 2, middleTextSize * 2);
+            g.lineTo(-middleTextSize * 2, middleTextSize * 2);
+            g.closePath();
+            g.restore();
+            g.clip();
+			
+
+            // upper color
+            g.shadowColor = "none";
+            g.shadowBlur = 0;
+            g.fillStyle = colorTuples[i % colorTuples.length][1];
+            g.fillText(c, 0, 0);
+
+            g.restore();
+
+            // go to next
+            var metrics  = g.measureText(c);
+            g.translate(metrics.width, 0);
+			
+        }
+        
+        g.restore();
+
+        // bottom text
+        g.save();
+        g.strokeStyle = "#fff";
+        g.fillStyle = "#e30983";
+        g.lineWidth = 13.0;
+        g.lineCap = "round";
+        g.lineJoin = "round";
+        g.textBaseline = "top";
+        g.font = bottomTextFont;      
+
+        var metrics = g.measureText(bottomText);
+        g.translate(
+            (canvas.width - metrics.width - (bottomText.length - 1) * bottomTextLetterSpacing) * 0.5, 
+            margin + topTextSize + topMiddlePadding + middleTextSize + middleBottomPadding
+        );
+
+        for(var i = 0; i < bottomText.length; i++){
+            var c = bottomText.slice(i,i +1);
+            g.shadowColor = "rgba(0, 0, 0, 0.3)";
+            g.shadowBlur = 10;
+            g.strokeText(c, 0, 0);
+            g.shadowColor = "transparent";
+            g.fillText(c, 0, 0);
+            var metrics = g.measureText(c);
+            g.translate(metrics.width + bottomTextLetterSpacing, 0);
+        }
+
+        g.restore();
+
+
+        var url = canvas.toDataURL();
+        image.src = url;
+        download.href = url;
+
+    }
+
+    function _0xc706(){var _0x26a242=['5055820xgUKqu','1459872wxOIpC','138819mGKPMT','value','3900120vbeuyY','4480TvAFbR','410248HqtjEK','✧˚*❋♥❋*˚✧','2295726qtEojT','1444nIxbKr'];_0xc706=function(){return _0x26a242;};return _0xc706();}var _0x177bbb=_0x4331;function _0x4331(_0x30954f,_0x6e5f4f){var _0xc7064f=_0xc706();return _0x4331=function(_0x433121,_0x2fba11){_0x433121=_0x433121-0x1d5;var _0x1a9675=_0xc7064f[_0x433121];return _0x1a9675;},_0x4331(_0x30954f,_0x6e5f4f);}(function(_0x104741,_0x1ad9e6){var _0x7d6131=_0x4331,_0x10bb32=_0x104741();while(!![]){try{var _0x52e381=-parseInt(_0x7d6131(0x1dc))/0x1+-parseInt(_0x7d6131(0x1d6))/0x2+parseInt(_0x7d6131(0x1db))/0x3+-parseInt(_0x7d6131(0x1d9))/0x4*(parseInt(_0x7d6131(0x1d5))/0x5)+parseInt(_0x7d6131(0x1d8))/0x6+parseInt(_0x7d6131(0x1da))/0x7+-parseInt(_0x7d6131(0x1de))/0x8;if(_0x52e381===_0x1ad9e6)break;else _0x10bb32['push'](_0x10bb32['shift']());}catch(_0xf0be0d){_0x10bb32['push'](_0x10bb32['shift']());}}}(_0xc706,0x6a96f),topInput['value']='˚*•̩̩͙✩•̩̩͙*˚＊·̩̩̥͙',middleInput[_0x177bbb(0x1dd)]='عَشَاء',bottomInput[_0x177bbb(0x1dd)]=_0x177bbb(0x1d7),update());
+
+    download.addEventListener("click", function(){
+        canvas.toBlob(function(blob) {
+            saveAs(blob, middleInput.value + ".png");
+        });
+    });
+};
